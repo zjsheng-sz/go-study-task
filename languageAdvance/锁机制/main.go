27760e4faa655e95be2a8fa3f62391bb092f068e
zhong <>
2025-09-15 10:05:43 +0800

    atomic

--- languageAdvance/锁机制/main.go
+++ languageAdvance/锁机制/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 
-	calulate()
+	// calulate()
+	calulate2()
 }
 
 /*
@@ -39,3 +41,25 @@ func calulate() {
 /*
 使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
 */
+
+func calulate2() {
+
+	var res int64 = 0
+	var wg sync.WaitGroup
+	// var lock sync.Mutex
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 1000; i++ {
+				// lock.Lock()
+				atomic.AddInt64(&res, 1)
+				// lock.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("结果：", res)
+}
